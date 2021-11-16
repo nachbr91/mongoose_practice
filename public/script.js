e8d580478292a5cb7dd5983b8fc05f3141d3cdbf
nachbr91 <nabero91@gmail.com>
2021-11-16 13:03:37 +0100

    mongoose_practice

--- public/script.js
+++ public/script.js
@@ -1,19 +1,53 @@
+window.onload = () => {
+  const createStudentButton = document.getElementById('create-student-button');
 
-const createStudentButton = document.getElementById('create-student-button');
+  //Event listener del botón para crear un nuevo alumno
+  if (createStudentButton) {
+    createStudentButton.addEventListener('click', async () => {
+      const name = document.getElementById('name').value;
+      const lastName = document.getElementById('lastName').value;
+      const age = document.getElementById('age').value;
+      const classroom = document.getElementById('class').value;
+      const idioma = document.getElementById('idioma').value;
 
-createStudentButton.addEventListener('click', () => {
-  const name = document.getElementById('name').value;
-  const lastName = document.getElementById('lastName').value;
-  const age = document.getElementById('age').value;
-  const classroom = document.getElementById('class').value;
-  const idioma = document.getElementById('idioma').value;
+      const allInputs = { name, lastName, age, class: classroom, idioma };
 
-  const allInputs = {name, lastName, age, class: classroom, idioma};
+      //Ruta POST con axios
+      await axios({
+        method: 'POST',
+        url: '/new-student',
+        data: allInputs,
+      });
+      window.location.assign('/all-students');
 
-  // Enviar información del frontend al backend
-  axios ({
-    method: 'POST',
-    url: 'http://localhost:3000/new-student',
-    data: allInputs
-  })
-});
+      //Ruta GET con axios
+      // axios('http://localhost:3000/new-student')
+    });
+  }
+
+  //Event listener para eliminar un usuario
+  const deleteStudentButton = document.getElementById("delete-student");
+  const editStudentButton = document.getElementById("edit-student");
+  if (deleteStudentButton && editStudentButton) {
+    deleteStudentButton.addEventListener("click", async () => {
+      const studentId = deleteStudentButton.getAttribute("data");
+      await axios({
+        method: "DELETE",
+        url: `/student/${studentId}`,
+      });
+      window.location.assign("/all-students");
+    });
+
+    editStudentButton.addEventListener("click", async () => {
+      const studentId = editStudentButton.getAttribute("data");
+      const classroom = document.getElementById("class-edit").value;
+      const pendingBills = document.getElementById("pendingBills-edit").value;
+      await axios({
+        method: "PUT",
+        url: `/edit-student/${studentId}`,
+        data: {class: classroom, pendingBills}
+      })
+      window.location.assign(`/student/${studentId}`);
+    });
+  }
+};
--- server.js
+++ server.js
@@ -14,10 +14,13 @@ const Student = require('./models/Student.js');
 //Connection to DB
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb+srv://<user>:<password>@cluster0.r4vv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      'mongodb+srv://Nacho:12345@cluster0.r4vv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log(chalk.blue('Conectado a Mongo'));
   } catch (err) {
     console.log('Error:', err);
@@ -41,34 +44,64 @@ app.get('/', (req, res) => {
   res.render('home.hbs');
 });
 
-app.get('/student/:id', async (req, res)=>{
+app.get('/student/:id', async (req, res) => {
   try {
-    const studentInfoFromDatabase = await Student.findById(
-      req.params.id,
-      {name: 1, lastName: 1, age: 1, class: 1, idioma: 1}
-    )
-    res.render('student.hbs', studentInfoFromDatabase)
-  } catch(err){
-    res.render('error.hbs', {errorMsg: "El ID proporcionado no corresponde con ningún alumno."})
+    const studentInfoFromDatabase = await Student.findById(req.params.id, {
+      name: 1,
+      lastName: 1,
+      age: 1,
+      class: 1,
+      idioma: 1,
+    });
+    res.render('student.hbs', studentInfoFromDatabase);
+  } catch (err) {
+    res.render('error.hbs', {
+      errorMsg: 'El ID proporcionado no corresponde con ningún alumno.',
+    });
   }
-})
+});
 
 //Llamada a la base de datos y enviarlo al front-end
 app.get('/all-students', async (req, res) => {
-  const allStudents = await Student.find({}, {name: 1, lastName: 1})
-  res.render('allStudents.hbs', {allStudents});
+  const allStudents = await Student.find({}, { name: 1, lastName: 1, _id: 1 });
+  res.render('allStudents.hbs', { allStudents });
 });
 
 app.get('/new-student', (req, res) => {
-  res.render('newStudent.hbs')
+  res.render('newStudent.hbs');
 });
 
+//Ruta POST para crear un nuevo estudiante
 app.post('/new-student', async (req, res) => {
   try {
-    const createdStudent = await Student.create(req.body)
-    console.log(createdStudent);
-  } catch(err) {
-    console.log('Error:', err)
+    const createdStudent = await Student.create(req.body);
+    res.send(createdStudent);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Ruta DELETE para eliminar un alumno por su ID
+
+app.delete('/student/:id', async (req, res) => {
+  try {
+    const deletedStudent = await Student.findByIdAndDelete(req.params.id);
+    res.send(deletedStudent);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Ruta PUT para editar un alumno
+app.put('/edit-student/:id', async (req, res) => {
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.send(updatedStudent);
+  } catch (err) {
+    console.log(err);
   }
 });
 
